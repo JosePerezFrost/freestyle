d3839f503ad30afe9246d9742012d00aa3c13f0a
nicol <nicol@SALAZARNICOLAS>
2018-09-14 11:51:43 -0500

    Cambios Conexion

--- Edu.Sena.Datos/Conexion.cs
+++ Edu.Sena.Datos/Conexion.cs
@@ -1,13 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Edu.Sena.Datos
 {
     public class Conexion
     {
-        // hola perros
+        public static SqlConnection cadena;
+        public static SqlCommand comando;
+        public static SqlDataReader lecturaDatos;
+        public static DataTable Tabla;
+        public string cadenaConexion;
+        public string _mensaje;
+
+        public static void Conectar()
+        {
+            cadena = new SqlConnection(@"Data Source=SALAZARNICOLAS\SQLEXPRESS;Initial Catalog=db_sifreestyle;Integrated Security=True");
+            comando = new SqlCommand();
+            comando.Connection = cadena;
+        }
+        public static void Ejecutar(string Sentencia)
+        {
+            Conectar();
+            comando.CommandText = Sentencia;
+            cadena.Open();
+            comando.ExecuteNonQuery();
+            cadena.Close();
+        }
+        public static DataTable consultar(string Sentencia)
+        {
+            Conectar();
+            comando.CommandText = Sentencia;
+            cadena.Open();
+            lecturaDatos = comando.ExecuteReader();
+            Tabla = new DataTable();
+            Tabla.Load(lecturaDatos);
+            lecturaDatos.Close();
+            return Tabla;
+        }
     }
 }
--- Edu.Sena.Logica/Rol.cs
+++ Edu.Sena.Logica/Rol.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Edu.Sena.Datos;
+using System.Data;
 
 namespace Edu.Sena.Logica
 {
@@ -10,8 +12,28 @@ namespace Edu.Sena.Logica
     {
         private int id;
         private string tipo;
+        public string sentencia;
 
         public int Id { get => id; set => id = value; }
         public string Tipo { get => tipo; set => tipo = value; }
+
+        public void Insertar_Rol()
+        {
+            sentencia = "insert into dbo.roles (idRol, tipo) values ("+ id + ",'"+tipo+"') ";
+            Conexion.Ejecutar(sentencia);
+        }
+
+        public void Actualizar_Rol()
+        {
+            sentencia = "UPDATE dbo.roles SET tipo='" + tipo + "'";
+            Conexion.Ejecutar(sentencia);
+        }
+
+        public  DataTable consultar_rol()
+        {
+            sentencia = "SELECT * FROM dbo.roles";
+            return Conexion.consultar(sentencia);
+        }
+
     }
 }
